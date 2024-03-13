672d7ef92d6889f1612095e260f488429746c9b7
casual <63418759+casualize@users.noreply.github.com>
2024-03-13 21:02:37 +0200

    first commit

---
+++ gmod-animations-test/compile.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Source SDK Base 2013 Multiplayer\bin\studiomdl.exe" -game "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod" "C:\SUBSTITUTETHISWITHYOURDIRECTORY\player_male_anims_m_anm.qc"
+PAUSE
